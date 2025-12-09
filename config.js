97a609fe1d85539dd0ef8294042d453235bb1acd
kaveeshamihiranga293-byte <kaveeshamihiranga293@gmail.com>
2025-12-09 16:17:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/kaveeshamihiranga293-byte/Kavi-Mishu-md/blob/main/image/WhatsApp%20Image%202025-12-09%20at%203.07.32%20PM.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Kavi-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
